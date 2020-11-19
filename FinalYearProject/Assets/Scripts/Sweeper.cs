a2f9c56a0ecc5ed0f6cbf6341fcf40943a2c2ab2
Dom Sleightholme <domsleightholme7@gmail.com>
2020-11-19 17:02:38 +0000

    Implemented Sweeper
    Using the mode I had previously made in Blender, I managed to create the basic function for the sweeper in game, I made the sweeper rotate constantly and also gain speed over time but this setting can be changed if needed.

---
+++ FinalYearProject/Assets/Scripts/Sweeper.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sweeper : MonoBehaviour
+{
+    [Header("Speed")]
+    [Range(0, 200)]
+    public float RotationSpeed;
+    public float minSpeed;
+    public float maxSpeed;
+
+    [Header("Increase Speed")]
+    public float increaseMulitpler;
+    public bool canIncreaseSpeed;
+
+    private void Update()
+    {
+        Rotate();
+
+        if (canIncreaseSpeed)
+        {
+            IncreaseSpeed();
+        }
+    }
+
+    void Rotate()
+    {
+        transform.Rotate(0,0, RotationSpeed * Time.deltaTime);
+    }
+
+    void IncreaseSpeed()
+    {
+        if(RotationSpeed < maxSpeed)
+        {
+            RotationSpeed += increaseMulitpler * Time.deltaTime;
+        }
+    }
+}
