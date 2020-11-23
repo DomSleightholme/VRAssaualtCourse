f95fd5e412c4797b08721dfbf64b2cecfbcd0550
Dom Sleightholme <domsleightholme7@gmail.com>
2020-11-23 00:20:56 +0000

    Pendulum Modelled and Implemented
     I have designed a pendulum model to implement into my game, all though this is model will be used in prototypes coming up, I will updated this model at a later date.  I made a script which rotates the pendulum to a certain angle then rotates in the opposite direction to capture the movement of a pendulum.

---
+++ FinalYearProject/Assets/Scripts/Pendulum.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pendulum : MonoBehaviour
+{
+    [Range(0, 3)]
+    public float RotationSpeed;
+
+    public float MaxLeft;
+    public int MaxRight;
+    float Rotation;
+
+    public bool Left;
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+       if(Left == true)
+        {
+            transform.Rotate(new Vector3(0, 0, -RotationSpeed));
+        }
+        else
+        {
+            transform.Rotate(new Vector3(0, 0, RotationSpeed));
+        }
+
+        if (transform.eulerAngles.z >= MaxRight && transform.eulerAngles.z <= MaxRight + 2)
+        {
+            Left = true;
+        }
+
+        if(transform.eulerAngles.z >= MaxLeft && transform.eulerAngles.z <= MaxLeft + 2)
+        {
+            Left = false;
+        }
+    }
+}
