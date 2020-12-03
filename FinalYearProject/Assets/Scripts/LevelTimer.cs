9c029defd5d8cd243558cc54bbf934c5e96dee2e
Dom Sleightholme <domsleightholme7@gmail.com>
2020-12-03 15:47:35 +0000

    Level Timer
    I have been working on a level timer to count how long the player will take to complete a level, this timer will be used in most of the levels so it is a huge part of the fun!

---
+++ FinalYearProject/Assets/Scripts/LevelTimer.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelTimer : MonoBehaviour
+{
+    [Header("Timer")]
+    private float timer;   
+    public int minutes;
+    public int seconds;
+    public bool timeRunning = false;
+
+    [Header("UI")]
+    public Text minutesUI;
+    public Text secondsUI;
+    string minUI;
+    string secUI;
+
+    private void Update()
+    {
+        if(timeRunning == true)
+        {
+            timer += Time.deltaTime;
+        }
+
+        TimerOn();
+        TimerUI();
+    }
+
+    void TimerOn()
+    {
+        minUI = Mathf.Floor(timer / 60).ToString("00");
+        secUI = Mathf.RoundToInt(timer % 60).ToString("00");
+
+        minutes = int.Parse(minUI);
+        seconds = int.Parse(secUI);
+    }
+
+    void TimerUI()
+    {
+        minutesUI.text = minUI.ToString();
+        secondsUI.text = secUI.ToString();
+    }
+}
