51225e50f7c57578807ab6eceeef456b627bb939
Dom Sleightholme <domsleightholme7@gmail.com>
2021-03-23 22:12:06 +0000

    Moving Platform Implementation

---
+++ Project/VRWipeout/Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    public GameObject FirstPosition;
+    public GameObject SecondPosition;
+
+    private Vector3 Pos1;
+    private Vector3 Pos2;
+
+    public float Speed = 1;
+
+    private void Start()
+    {
+        Pos1 = FirstPosition.transform.position;
+        Pos2 = SecondPosition.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.Lerp(Pos1, Pos2, Mathf.PingPong(Time.time * Speed, 1));
+    }
+}
