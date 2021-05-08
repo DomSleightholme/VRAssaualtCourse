092a759c295a66dcf43541b6b3ccab82edad5ffd
Dom Sleightholme <domsleightholme7@gmail.com>
2021-05-09 00:39:33 +0100

    Improved Camera Vignette

---
+++ Project/VRWipeout/Assets/Scripts/Break.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Break : MonoBehaviour
+{
+    [Header("Timer Limit")]
+    public float TimeLimit;
+
+    [Header("Timer")]
+    public float minTimer;
+    public float secTimer;
+    public bool timerOn;
+
+    [Header("Transforms")]
+    public GameObject UI;
+
+    private static Break @break;
+    void Awake()
+    {
+        DontDestroyOnLoad(this);
+
+        if (@break == null)
+        {
+            @break = this;
+        }
+        else
+        {
+            DestroyObject(this.gameObject);
+        }
+
+        minTimer = TimeLimit;
+    }
+
+    private void Update()
+    {
+        if (timerOn)
+        {
+           if(minTimer == 0.0f)
+           {
+                TimerEnd();
+           }
+            else
+            {
+                if (secTimer > 0)
+                {
+                    secTimer -= Time.deltaTime;
+                }
+                else
+                {
+                    secTimer = 60;
+                    minTimer--;
+                }
+            }
+        }
+    }
+
+    public void TimerEnd()
+    {
+        UI.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    public void CloseUi()
+    {
+        UI.SetActive(false);
+        Time.timeScale = 1;
+    }
+}
---
+++ Project/VRWipeout/Assets/Scripts/VR Player/CameraVignette.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class CameraVignette : MonoBehaviour
+{
+  
+}
